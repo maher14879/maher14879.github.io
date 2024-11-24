4fc89c7cd86b6de9043104e0d7038dda5a189795
maher14879 <maherelyoussef1@gmail.com>
2024-11-24 14:19:34 +0100

    change over to javascript

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # maherelyoussef
 repository for my personal website
-
-Using Python 3.12.7
-
-flask-3.1.0
---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const contentDiv = document.getElementById('dynamic-content');
+    contentDiv.innerHTML = '<p>This content was added using JavaScript!</p>';
+});

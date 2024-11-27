9301bf4db089e9f5e0b95a926813de81e660b562
maher14879 <maherelyoussef1@gmail.com>
2024-11-27 16:10:21 +0100

    lets see

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const boxShadow = `0 0 7px 1px grey, 0 0 7px 1px grey`;
 
 class Dot {
     constructor(x, y, color = 'white') {
-        this.scale = Math.random() ** 2
+        this.scale = Math.random() ** 2;
         this.dot = document.createElement('div');
         this.dot.style.position = 'absolute';
         this.dot.style.width = `${this.scale * 5 + 1}px`;
         this.dot.style.height = `${this.scale * 5 + 1}px`;
         this.dot.style.backgroundColor = color;
         this.dot.style.pointerEvents = 'none';
-        this.dot.style.boxShadow = boxShadow
+        this.dot.style.boxShadow = boxShadow;
 
         this.posX = x;
         this.posY = y;
@@ -26,22 +26,26 @@ class Dot {
     add_pos(x, y) {
         this.posX -= x * this.scale;
         this.posY -= y * this.scale;
-    
+
         if (this.posY >= window.innerHeight) {
             this.posY = 1;
         } else if (this.posY <= 0) {
             this.posY = window.innerHeight - 1;
         }
-    
+
         if (this.posX >= window.innerWidth) {
             this.posX = 1;
         } else if (this.posX <= 0) {
             this.posX = window.innerWidth - 1;
         }
-    
+
         this.updatePosition();
     }
 }
+
+let dots = [];
+let deltaPosition_x = 0;
+let deltaPosition_y = 0;
 
 function createRandomDot() {
     const randomX = Math.random() * window.innerWidth;
@@ -51,8 +55,8 @@ function createRandomDot() {
 }
 
 document.addEventListener('mousemove', (event) => {
-    deltaPosition_x = (event.clientX - window.innerWidth / 2)
-    deltaPosition_y = (event.clientY - window.innerHeight / 2)
+    deltaPosition_x = (event.clientX - window.innerWidth / 2);
+    deltaPosition_y = (event.clientY - window.innerHeight / 2);
 });
 
 function animateDots() {
@@ -76,23 +80,15 @@ function loadDotsFromStorage() {
             createRandomDot();
         }
     } else {
-        storedDots.forEach(({ x, y }) => dots.push(new Dot(x, y)));
+        storedDots.forEach(({ x, y, scale }) => {
+            const dot = new Dot(x, y);
+            dot.scale = scale;
+            dots.push(dot);
+        });
     }
 }
 
-
-function saveDotsToStorage() {
-    const dotData = dots.map(dot => ({ x: dot.posX, y: dot.posY, scale: dot.scale }));
-    localStorage.setItem('dots', JSON.stringify(dotData));
-}
-
-function loadDotsFromStorage() {
-    const storedDots = JSON.parse(localStorage.getItem('dots') || '[]');
-    storedDots.forEach(({ x, y }) => dots.push(new Dot(x, y)));
-}
-
 loadDotsFromStorage();
-loadDeltaPosition();
 window.addEventListener('beforeunload', () => {
     saveDotsToStorage();
     localStorage.setItem('deltaPosition', JSON.stringify({ x: deltaPosition_x, y: deltaPosition_y }));

d04b87e47f587d8841870b200426276d9ff997cf
maher14879 <maherelyoussef1@gmail.com>
2025-01-16 09:51:50 +0100

    i think we gucci

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const boxShadow = `0 0 7px 1px grey, 0 0 7px 1px grey`;
 class Dot {
     constructor(x, y, scale, color = 'white') {
         this.scale = scale;
-        this.speed = 0.01
+        this.speed = 0.1
         this.dot = document.createElement('div');
         this.dot.style.position = 'absolute';
         this.dot.style.width = `${this.scale * 3 + 1}px`;
@@ -57,7 +57,7 @@ function createRandomDot() {
 
 let lastMouseMove = 0;
 const mouseMoveDelay = 10; // Throttle mousemove event to every 10ms
-const mouseSmooth = 1
+const mouseSmooth = 0.01
 
 document.addEventListener('mousemove', (event) => {
     targetX = event.clientX - window.innerWidth / 2;

d9cdc33925cc5b0d6ba0ddeef99c365412ed34ef
maher14879 <maherelyoussef1@gmail.com>
2025-04-10 09:03:35 +0200

    changed index

--- script.js
+++ script.js
@@ -132,3 +132,9 @@ function replaceWordsWithLinks(text) {
     });
     return text;
 }
+
+document.addEventListener('mousemove', function (e) {
+    const cursor = document.querySelector('.custom-cursor');
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+  });

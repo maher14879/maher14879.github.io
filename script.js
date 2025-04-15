05f99c9d1e052e911b8b96ad71400dd1ab5eb65c
maher14879 <maherelyoussef1@gmail.com>
2025-04-15 14:59:46 +0200

    test new midi player

--- script.js
+++ script.js
@@ -270,10 +270,10 @@ function animateDots() {
                 const track = tracks[i];
                 const period = track.getCurrentPeriod(nowTime);
                 if (period != null) {
-                    const term1 = (dot.posX - track.posX + Math.random() - 0.5) * period
-                    const term2 = (dot.posY - track.posY + Math.random() - 0.5) * period
-                    force_x += term1 * Math.sin(term1) * Math.sin(term2) * waveSmooth;
-                    force_y += term2 * Math.cos(term1) * Math.cos(term2) * waveSmooth;
+                    const term1 = (dot.posX - track.posX + Math.random() - 0.5)
+                    const term2 = (dot.posY - track.posY + Math.random() - 0.5)
+                    force_x += term1 * Math.sin(term1 * period) * Math.sin(term2 * period) * waveSmooth;
+                    force_y += term2 * Math.cos(term1 * period) * Math.cos(term2 * period) * waveSmooth;
                     if (spawnDot > spawnSpeed) {
                         dots.push(new Dot(track.posX, track.posY, Math.random() ** 2));
                     }

1692212489c67a57d79ffe06765cca037bf06720
maher14879 <maherelyoussef1@gmail.com>
2025-07-13 07:57:39 +0200

    added prototype

--- isoarcade.js
+++ isoarcade.js
@@ -614,7 +614,7 @@ class IsoArcade {
 // Usage:
 const textureSheet = new Image();
 textureSheet.crossOrigin = 'anonymous';
-textureSheet.src = 'assets/texture_sheet.png';
+textureSheet.src = 'assets/pixel_art/texture_sheet.png';
 
 const textureArray = [
     [0, 0],
